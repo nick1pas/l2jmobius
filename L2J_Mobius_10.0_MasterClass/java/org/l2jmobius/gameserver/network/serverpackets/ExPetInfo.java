7b80ae6716320d5b96f03aa5908475bd443dbb9f
MobiusDevelopment <mobius@cyber-wizard.com>
2022-03-06 15:00:32 +0200

    NpcInfoType VISUAL_STATE is written as an integer. Thanks to MacuK.

--- L2J_Mobius_10.0_MasterClass/java/org/l2jmobius/gameserver/network/serverpackets/ExPetInfo.java
+++ L2J_Mobius_10.0_MasterClass/java/org/l2jmobius/gameserver/network/serverpackets/ExPetInfo.java
@@ -357,7 +357,7 @@ public class ExPetInfo extends AbstractMaskPacket<NpcInfoType>
 		}
 		if (containsMask(NpcInfoType.VISUAL_STATE))
 		{
-			packet.writeC(_statusMask);
+			packet.writeD(_statusMask);
 		}
 		if (containsMask(NpcInfoType.ABNORMALS))
 		{
