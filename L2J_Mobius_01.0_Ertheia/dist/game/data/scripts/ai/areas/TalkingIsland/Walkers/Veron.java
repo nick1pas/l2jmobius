5337ea688533dea5b8060e1e1159a4fc96c0c5ff
MobiusDevelopment <mobius@cyber-wizard.com>
2022-07-25 14:29:58 +0300

    Addition of Veron AI. Contributed by Index.

--- L2J_Mobius_01.0_Ertheia/dist/game/data/scripts/ai/areas/TalkingIsland/Walkers/Veron.java
+++ L2J_Mobius_01.0_Ertheia/dist/game/data/scripts/ai/areas/TalkingIsland/Walkers/Veron.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of the L2J Mobius project.
- *
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package ai.areas.TalkingIsland;
+package ai.areas.TalkingIsland.Walkers;
 
 import org.l2jmobius.gameserver.enums.ChatType;
 import org.l2jmobius.gameserver.model.actor.Npc;
@@ -24,17 +24,17 @@ import org.l2jmobius.gameserver.network.NpcStringId;
 import ai.AbstractNpcAI;
 
 /**
- * Kandra AI.
+ * Veron AI.
  * @author Index
  */
-public class Kandra extends AbstractNpcAI
+public class Veron extends AbstractNpcAI
 {
 	// NPC
-	private static final int KANDRA = 33111;
+	private static final int VERON = 33112;
 	
-	private Kandra()
+	private Veron()
 	{
-		addSpawnId(KANDRA);
+		addSpawnId(VERON);
 	}
 	
 	@Override
@@ -42,7 +42,7 @@ public class Kandra extends AbstractNpcAI
 	{
 		if (event.equals("NPC_SHOUT"))
 		{
-			npc.broadcastSay(ChatType.NPC_GENERAL, NpcStringId.IMPRESSIVE_LOOKING_STATUES);
+			npc.broadcastSay(ChatType.NPC_GENERAL, NpcStringId.SHILEN);
 			startQuestTimer("NPC_SHOUT", (10 + getRandom(5)) * 1000, npc, null);
 		}
 		return super.onAdvEvent(event, npc, player);
@@ -57,6 +57,6 @@ public class Kandra extends AbstractNpcAI
 	
 	public static void main(String[] args)
 	{
-		new Kandra();
+		new Veron();
 	}
 }
