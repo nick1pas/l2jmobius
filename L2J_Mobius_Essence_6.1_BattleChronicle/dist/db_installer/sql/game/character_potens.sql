3cc27258358cce731938d74a5397cbf9cec81d82
MobiusDevelopment <mobius@cyber-wizard.com>
2022-09-17 15:44:20 +0300

    Removal of 10.1_MasterClass and Essence_6.1_BattleChronicle.

--- L2J_Mobius_Essence_6.1_BattleChronicle/dist/db_installer/sql/game/character_potens.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `character_potens`;
-CREATE TABLE IF NOT EXISTS `character_potens` (
-  `charId` INT UNSIGNED NOT NULL DEFAULT 0,
-  `enchant_level` INT NOT NULL DEFAULT 0,
-  `enchant_exp` INT NOT NULL DEFAULT 0,
-  `poten_id` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`charId`,`poten_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
